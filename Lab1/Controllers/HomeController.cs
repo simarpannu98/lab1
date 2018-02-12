821700746cb1d17f32230e2387cf996cb8ed091f
simarpannu98 <pannusimar98@gmail.com>
2018-02-12 14:55:26 -0500

    first

---
+++ Lab1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is the about page ";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Lab1/Controllers/StoreController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson1.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Store
+        public ActionResult Details(string product)
+        {
+            if (product == null)
+            {
+                return View("error!");
+            }
+
+
+
+            ViewBag.Product = product;
+            return View(); 
+        }
+    }
+}
